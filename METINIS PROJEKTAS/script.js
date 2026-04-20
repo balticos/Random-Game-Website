00bb5ba1049b514a3a11bd1f0538b144f1ea41e5
balticos <95939526+balticos@users.noreply.github.com>
2026-04-20 12:16:18 +0300

    Delete METINIS PROJEKTAS directory

--- METINIS PROJEKTAS/script.js
+++
@@ -1,8 +0,0 @@
-const playButton = document.getElementById("play-button");
-if (playButton) {
-  playButton.addEventListener("click", function () {
-    const templates = ["flappy.html", "pong.html", "snake-controls.html"];
-    const randomTemplate = templates[Math.floor(Math.random() * templates.length)];
-    window.location.href = randomTemplate;
-  });
-}
